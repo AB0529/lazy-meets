0fdf4af0d79faed855a0c8ef658e44c35c9417e2
AB0529 <supercrab115@gmai.com>
2021-02-10 02:27:42 -0500

    Small fix which didn't work

--- src/util.go
+++ src/util.go
@@ -327,7 +327,7 @@ func StartMeet(class *Class, config *Config) {
 			if strings.Contains(curURL, "&born&hs") {
 				config.Leave = oldLeaveCondition
 				Info(prompter.Red.Sprint("Ended ") + "breakout room")
-				time.Sleep(time.Minute)
+				time.Sleep(time.Minute * 1)
 				continue
 			}
 
